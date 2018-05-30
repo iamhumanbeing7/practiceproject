ea785bbff5bf8760282a31b6dc1db242683ba9d4
iamhumanbeing7 <35964704+iamhumanbeing7@users.noreply.github.com>
2018-05-31 02:55:31 +0800

    2015-05-31

--- src/com/sc/persistence/DutyStaffGenerator.java
+++ src/com/sc/persistence/DutyStaffGenerator.java
@@ -1,8 +1,20 @@
 package com.sc.persistence;
 
 import com.sc.domain.DASolution;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.model.SharedStringsTable;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Iterator;
 
 public class DutyStaffGenerator {
 
@@ -14,7 +26,27 @@ public class DutyStaffGenerator {
 
     private void createDutyList(DASolution dasolution, int planningWeekSize){}
 
-    private void createStaffList(DASolution dasolution, int staffSize){}
+    private void createStaffList(DASolution dasolution, int staffSize){
+        try {
+            File excelFile = new File("Staff_List.xlsx");
+            FileInputStream fis = new FileInputStream(excelFile);
+            XSSFWorkbook xWorkBook = new XSSFWorkbook(fis);
+            XSSFSheet xSheet = xWorkBook.getSheetAt(0);
+            Iterator<Row> rowIterator = xSheet.iterator();
+            while(rowIterator.hasNext()){
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()){
+                    Cell cell = cellIterator.next();
+
+                }
+            }
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 
 
 }
--- src/com/test/testmain.java
+++ src/com/test/testmain.java
@@ -1,14 +1,72 @@
 package com.test;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.formula.eval.NotImplementedException;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
 
 public class testmain {
     public static void main(String[] argv) throws Exception {
-        BigInteger bi1 = new BigInteger("1234567890123456890");
-        int exponent = 2;
-
-        BigInteger b2 = bi1.pow(exponent);
-        System.out.println(b2);
-        System.out.println(bi1.multiply(bi1));
+        Date dateCellValue;
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        try {
+            File excelFile = new File("D:\\IntellijIdea_space\\practiceproject\\src\\com\\sc\\persistence\\Staff_List.xlsx");
+            FileInputStream fis = new FileInputStream(excelFile);
+            XSSFWorkbook xWorkBook = new XSSFWorkbook(fis);
+            XSSFSheet xSheet = xWorkBook.getSheetAt(0);
+            DataFormatter dataFormatter = new DataFormatter();
+//            FormulaEvaluator evaluator = xWorkBook.getCreationHelper().createFormulaEvaluator();
+            String cellValue = null;
+            Iterator<Row> rowIterator = xSheet.rowIterator();
+            int counter = 0;
+            while(rowIterator.hasNext()){
+                if (counter == 0) {
+                    System.out.print("ignore headers \r\n");
+                    rowIterator.next();
+                    counter++;
+                    continue;
+                } else if (counter >3) break;
+                counter++;
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()){
+                    Cell cell = cellIterator.next();
+                    if (cell.getCellTypeEnum() == CellType.NUMERIC){
+                        if (DateUtil.isCellDateFormatted(cell)) {
+                            cellValue = "Date Type: \r\n  " + simpleDateFormat.format(cell.getDateCellValue());
+                        } else {
+                            cellValue = "Numeric Type: \r\n  " + (int)cell.getNumericCellValue();
+                        }
+                    } else {
+                        cellValue = "String Type: \r\n  " + dataFormatter.formatCellValue(cell);
+                    }
+//                    cellValue = dataFormatter.formatCellValue(cell, evaluator);
+                    System.out.println(cellValue);
+                }
+            }
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        } catch (NotImplementedException exception){
+            exception.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            System.out.println("completed finally!");
+        }
     }
 }
